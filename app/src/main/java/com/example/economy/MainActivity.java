38d71fe0615842bfece66272b2ef27de11957c2b
Igancio Fernández <ignacio.f.diaz98@gmail.com>
2020-12-02 15:05:46 +0100

    Prueba e introduccion de los on

--- app/src/main/java/com/example/economy/MainActivity.java
+++ app/src/main/java/com/example/economy/MainActivity.java
@@ -10,5 +10,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
