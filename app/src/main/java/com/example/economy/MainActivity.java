0ff002cdbc09238834cfbe93190e0dc5f5ec07c3
Igancio Fernández <ignacio.f.diaz98@gmail.com>
2020-12-23 13:27:01 +0100

    Integracion de google analytic

--- app/src/main/java/com/example/economy/MainActivity.java
+++ app/src/main/java/com/example/economy/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtiene la instancia de FirebaseAnalytics.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
     }
 
